bb6daac87445ec1ba153d4a11e61fa1b5ae4f8ae
Rick G <shadsterwolf1@gmail.com>
2018-09-30 20:13:22 -0500

    1.0

---
+++ MhwMeshUpdate/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace MhwMeshUpdate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxModFile = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxMeshFile = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxOffset = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxHops = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Model file to update";
+            // 
+            // tbxModFile
+            // 
+            this.tbxModFile.Location = new System.Drawing.Point(13, 30);
+            this.tbxModFile.Name = "tbxModFile";
+            this.tbxModFile.Size = new System.Drawing.Size(230, 20);
+            this.tbxModFile.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mesh data";
+            // 
+            // tbxMeshFile
+            // 
+            this.tbxMeshFile.Location = new System.Drawing.Point(13, 89);
+            this.tbxMeshFile.Name = "tbxMeshFile";
+            this.tbxMeshFile.Size = new System.Drawing.Size(230, 20);
+            this.tbxMeshFile.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(249, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 171);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Force Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Offset";
+            // 
+            // tbxOffset
+            // 
+            this.tbxOffset.Location = new System.Drawing.Point(142, 174);
+            this.tbxOffset.Name = "tbxOffset";
+            this.tbxOffset.Size = new System.Drawing.Size(73, 20);
+            this.tbxOffset.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(236, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Blocksize";
+            // 
+            // tbxHops
+            // 
+            this.tbxHops.Location = new System.Drawing.Point(239, 173);
+            this.tbxHops.Name = "tbxHops";
+            this.tbxHops.Size = new System.Drawing.Size(72, 20);
+            this.tbxHops.TabIndex = 11;
+            this.tbxHops.Text = "32";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(124, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "0x";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(142, 129);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Make low poly";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 209);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbxHops);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbxOffset);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tbxMeshFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbxModFile);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "MHW (MT-F) Mesh Update";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxModFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxMeshFile;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxOffset;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxHops;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ MhwMeshUpdate/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MhwMeshUpdate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFile = new OpenFileDialog();
+            if (!(oFile.ShowDialog() == DialogResult.Cancel))
+            {
+                tbxModFile.Text = oFile.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFile = new OpenFileDialog();
+            if (!(oFile.ShowDialog() == DialogResult.Cancel))
+            {
+                tbxMeshFile.Text = oFile.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (tbxModFile.Text != "" && tbxMeshFile.Text != "" )
+            {
+                byte[] modFile, meshFile;
+                try
+                {
+                    modFile = System.IO.File.ReadAllBytes(tbxModFile.Text);
+                    meshFile = System.IO.File.ReadAllBytes(tbxMeshFile.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                    return;
+                }
+                uint vertexCount = GetVertexCount(modFile);
+                //MessageBox.Show("vertexCount: " + vertexCount);
+                uint meshCount = (uint)meshFile.Length / 16;
+                //MessageBox.Show("meshCount: " + meshCount);
+                if (vertexCount != meshCount)
+                {
+                    MessageBox.Show("Error: Mod Vertex and Mesh count do not match!" + "\n\n" + "vertexCount: " + vertexCount + "\n" + "meshCount: " + meshCount);
+                }
+
+                uint vertexOffset = GetVertexOffset(modFile);
+                //MessageBox.Show("vertexOffset: " + vertexOffset);
+                UpdateMeshData(tbxModFile.Text, vertexOffset, vertexCount, meshFile);
+                MessageBox.Show("Done!");
+            }
+            else
+            {
+                MessageBox.Show("Need both files before updating!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (tbxModFile.Text != "" && tbxMeshFile.Text != "")
+            {
+                byte[] modFile, meshFile;
+                try
+                {
+                    modFile = System.IO.File.ReadAllBytes(tbxModFile.Text);
+                    meshFile = System.IO.File.ReadAllBytes(tbxMeshFile.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                    return;
+                }
+                uint meshCount = (uint)meshFile.Length / 16;
+                UpdateMeshData(tbxModFile.Text, (uint)int.Parse(tbxOffset.Text, System.Globalization.NumberStyles.HexNumber), (uint)int.Parse(tbxHops.Text), meshCount, meshFile);
+                MessageBox.Show("Done!");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (tbxModFile.Text != "")
+            {
+                byte[] modFile;
+                try
+                {
+                    modFile = System.IO.File.ReadAllBytes(tbxModFile.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                    return;
+                }
+                uint objectCount = ReadByte(modFile, 0x08);
+                //MessageBox.Show(objectCount.ToString());
+                uint objectOffset = ReadLong(modFile, 0x48, true);
+                UpdateLodLowPoly(tbxModFile.Text, objectOffset, 0x50, objectCount);
+                MessageBox.Show("Done!");
+            }
+        }
+
+        public static Boolean IsModFile(byte[] modFile)
+        {
+            return ReadLong(modFile, 0) == 1297040384;
+        }
+
+        public static uint GetVertexCount(byte[] modFile)
+        {
+            if (IsModFile(modFile))
+                return ReadLong(modFile, 0x0C, true);
+            else
+                return 0;
+        }
+
+        public static uint GetVertexOffset(byte[] modFile)
+        {
+            return ReadLong(modFile, 0x50, true);
+        }
+
+        public static void UpdateMeshData(string file, uint pos, uint vertexCount, byte[] meshFile)
+        {
+            UpdateMeshData(file, pos, 24, vertexCount, meshFile);
+        }
+
+        public static void UpdateMeshData(string file, uint pos, uint hops, uint vertexCount, byte[] meshFile)
+        {
+            byte[] modFile = System.IO.File.ReadAllBytes(file);
+            for (int i = 0; i < vertexCount; i++)
+            {
+                for(int j = 0; j < 16; j++)
+                {
+                    modFile[pos + j] = meshFile[(i * 0x10) + j];
+                }
+                pos += hops;
+            }
+
+            System.IO.StreamWriter stream = new System.IO.StreamWriter(file); //StreamWriter doesn't like to be used to overwrite data :(
+            stream.BaseStream.Write(modFile, 0, modFile.Length); //Just write the updated byte array
+            stream.Close(); //Save
+            stream.Dispose(); //End stream
+        }
+
+        public static void UpdateLodLowPoly(string file, uint pos, uint hops, uint objectCount)
+        {
+            byte[] modFile = System.IO.File.ReadAllBytes(file);
+
+            for (int i = 0; i < objectCount; i++)
+            {
+                uint lod = ReadLong(modFile, pos + 8, true);
+                //MessageBox.Show(lod.ToString());
+                if (lod >= 1 && lod <= 16)
+                {
+                    modFile[pos] = 0;
+                    modFile[pos+1] = 0;
+                    modFile[pos+2] = 0;
+                    modFile[pos+3] = 0;
+                    modFile[pos+8] = 0xFF;
+                    modFile[pos+9] = 0xFF;
+                }
+                else if (lod >= 17)
+                {
+                    modFile[pos] = 0;
+                    modFile[pos + 1] = 0;
+                    modFile[pos + 8] = 1;
+                    modFile[pos + 9] = 0;
+                }
+                pos += hops;
+            }
+           
+            System.IO.StreamWriter stream = new System.IO.StreamWriter(file); //StreamWriter doesn't like to be used to overwrite data :(
+            stream.BaseStream.Write(modFile, 0, modFile.Length); //Just write the updated byte array
+            stream.Close(); //Save
+            stream.Dispose(); //End stream
+        }
+
+        private static ushort Makeu16(byte b1, byte b2) //16-bit change (ushort, 0xFFFF)
+        {
+            return (ushort)(((ushort)b1 << 8) | (ushort)b2);
+        }
+
+        private static uint Makeu32(byte b1, byte b2, byte b3, byte b4) //32-bit change (uint, 0xFFFFFFFF)
+        {
+            return ((uint)b1 << 24) | ((uint)b2 << 16) | ((uint)b3 << 8) | (uint)b4;
+        }
+
+        private static byte[] Breaku16(ushort u16) //Byte change from 16-bits (byte, 0xFF, 0xFF)
+        {
+            return new byte[] { (byte)(u16 >> 8), (byte)(u16 & 0xFF) };
+        }
+
+        private static byte[] Breaku32(uint u32) //Byte change from 32-bits (byte, 0xFF, 0xFF, 0xFF, 0xFF)
+        {
+            return new byte[] { (byte)(u32 >> 24), (byte)((u32 >> 16) & 0xFF), (byte)((u32 >> 8) & 0xFF), (byte)(u32 & 0xFF) };
+        }
+
+        public static byte ReadByte(byte[] modFile, uint pos)
+        {
+            return modFile[pos];
+        }
+
+        public static uint ReadLong(byte[] modFile, uint pos)
+        {
+            return ReadLong(modFile, pos, false);
+        }
+        public static uint ReadLong(byte[] modFile, uint pos, Boolean swapEndian)
+        {
+            if (swapEndian)
+                return SwapLongEndian(Makeu32(modFile[pos], modFile[pos + 1], modFile[pos + 2], modFile[pos + 3]));
+            else
+                return Makeu32(modFile[pos], modFile[pos + 1], modFile[pos + 2], modFile[pos + 3]);
+        }
+
+        public static uint SwapLongEndian(uint value)
+        {
+            var b1 = (value >> 0) & 0xff;
+            var b2 = (value >> 8) & 0xff;
+            var b3 = (value >> 16) & 0xff;
+            var b4 = (value >> 24) & 0xff;
+
+            return b1 << 24 | b2 << 16 | b3 << 8 | b4 << 0;
+        }
+
+        public static uint SwapShortEndian(uint value)
+        {
+            var b1 = (value >> 0) & 0xff;
+            var b2 = (value >> 8) & 0xff;
+
+            return  b1 << 8 | b2 << 0;
+        }
+    }
+}
